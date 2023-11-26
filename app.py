47adc90e67bac53bb83a0f79732f9ced2657c5d5
Whimzee <pramitijagdish07@gmail.com>
2023-11-26 06:51:25 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import openai
+import os
+# from openai import OpenAI
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+st.title("Travel Agency ChatBot")
+
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            print(response)
+            if response.choices[0].finish_reason == None:
+                full_response += (response.choices[0].delta.content or "")
+                message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
